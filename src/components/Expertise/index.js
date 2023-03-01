a113090aa29a40d23bf1a0cfa94f02d82cdd10fa
melaniemdm <melanieborja.pro@gmail.com>
2023-03-01 15:10:56 +0100

    feature(js) text

--- src/components/Expertise/index.js
+++ src/components/Expertise/index.js
@@ -6,21 +6,21 @@ export default function Expertise(){
 <div className='expertiseDetail'>
     <div className='bloc1'>
     <div className='expertise1'>
-    <h3 className='expertiseTitleDetail'>SITE INTERNET </h3>
-    <div className='txtExpertise'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+    <h3 className='expertiseTitleDetail'>DESIGN GRAPHIQUE </h3>
+    <div className='txtExpertise'>Des designers graphiques expérimentés capables de créer des logos, des graphiques, des illustrations et des designs pour l'identité visuelle.</div>
 </div>
 <div className='expertise1'>
-    <h3 className='expertiseTitleDetail'>SITE INTERNET </h3>
-    <div className='txtExpertise'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+    <h3 className='expertiseTitleDetail'>DEVELOPPEMENT WEB </h3>
+    <div className='txtExpertise'> Des développeurs web capables de construire des sites web et des applications en utilisant des technologies web modernes comme HTML, CSS, JavaScript, Rect, Redux.</div>
 </div></div>
 <div className='bloc2'>
 <div className='expertise1'>
-    <h3 className='expertiseTitleDetail'>SITE INTERNET </h3>
-    <div className='txtExpertise'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+    <h3 className='expertiseTitleDetail'>MARKETING </h3>
+    <div className='txtExpertise'>Une bonne compréhension du marketing pour aider nos clients à se différencier de leurs concurrents et à atteindre leur public cible.</div>
 </div>
 <div className='expertise1'>
-    <h3 className='expertiseTitleDetail'>SITE INTERNET </h3>
-    <div className='txtExpertise'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+    <h3 className='expertiseTitleDetail'>CONNAISSANCE DES TENDANCES ACTUELLES </h3>
+    <div className='txtExpertise'>A la pointe les tendances actuelles en matière de design et de développement pour offrir des services de qualité supérieure et répondre aux besoins de nos clients..</div>
 </div></div>
 
 </div>
