7c0ff4fd89816ef8a8932cbaaa2e9281cde269cf
melaniemdm <melanieborja.pro@gmail.com>
2023-03-01 15:28:44 +0100

    feature(js & css) galery

--- src/components/Realisations/index.js
+++ src/components/Realisations/index.js
@@ -1,16 +1,34 @@
 import './style.scss';
 import tViolet from '../../assets/tViolet.png'
 export default function Realisations(){
-    return  <div className='realisationTitle'> 
+    return  <div className='realisationTitle'>
     <div className='triangleTitleRea'>  <img src={tViolet} alt='triangleBViolet'className='triangleViolet'></img><h1 className='reaTitle'>NOS REALISATIONS</h1>
  </div>
     <div className='realist'><div className="containerRea">
+    <div id="flexbox">
       
-<div className="rea1"></div>
-<div className="rea2"></div>
-<div className="rea3"></div>
-<div className="rea4"></div>
-<div className="rea5"></div>
+      <div class="column">
+      <img src="https://docs.google.com/uc?export=download&id=1U4Z-2GJcRDtIAmZt4ej0MOF_NvA0ntzS" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=1cfqbl28ezKPikIXApzZQOEUPwKm2AKrr" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=1I-bDFOoYZ-XUZkwDxRJmtiR_7DGVn_Eb" alt="Image" width="100%" />
+     
+     
+      </div>
+      <div class="column">
+      <img src="https://docs.google.com/uc?export=download&id=10PElUch6-oxpjga6Pe6PoHd2VUXZkUfu" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=1I-bDFOoYZ-XUZkwDxRJmtiR_7DGVn_Eb" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=1DU-S82guX9KooYyfDavkg1M_kpXAVrs1" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=10PElUch6-oxpjga6Pe6PoHd2VUXZkUfu" alt="Image" width="100%" />
+    
+      </div>
+      <div class="column">
+      <img src="https://docs.google.com/uc?export=download&id=1U4Z-2GJcRDtIAmZt4ej0MOF_NvA0ntzS" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=1cfqbl28ezKPikIXApzZQOEUPwKm2AKrr" alt="Image" width="100%" />
+      <img src="https://docs.google.com/uc?export=download&id=10PElUch6-oxpjga6Pe6PoHd2VUXZkUfu" alt="Image" width="100%" />
+     
+      </div>     
+      
+    </div>
 
 
     </div></div>
