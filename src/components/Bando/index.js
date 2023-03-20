f386051ff0293167b9711e2001e83ac39276ebdc
melaniemdm <melanieborja.pro@gmail.com>
2023-03-20 16:52:44 +0100

    fix(css&js) popup refacto(js) bando

--- src/components/Bando/index.js
+++ src/components/Bando/index.js
@@ -3,15 +3,13 @@ import bando from '../../assets/bando.png';
 export default function Bando(){
     return <div id='containerBando'>
 
-        {/* <div className="defileParent">
-
-<img className="defile" id="txtBando" src={bando} alt="bando"/>
-</div> */}
-
-<div class="marquee-rtl">
+<div className="marquee-rtl">
 <img className="defile" id="txtBando" src={bando} alt="bando"/>
       </div>
 
 </div>
 }
+
+
+
 
--- src/components/Realisations/index.js
+++ src/components/Realisations/index.js
@@ -15,8 +15,19 @@ import acoss from '../../assets/acoss.jpg';
 import nature from '../../assets/nature.jpg';
 import dsn from '../../assets/dsn.jpg';
 import simpliste from '../../assets/simpliste.jpg';
-
+import { SimpleModal, openModal } from 'mdm-simple-modal';
+import 'mdm-simple-modal/dist/index.modern.css';
+import { useState } from 'react';
+console.log(dsn)
 export default function Realisations(){
+  const [pictureProjet, setPictureProjet] = useState("");
+
+  function urlModal(src){
+    setPictureProjet(src);
+    openModal();
+  }
+
+console.log(pictureProjet)
     return  <div className='realisationTitle' id='realisation'>
     <div className='triangleTitleRea'>  <img src={tViolet} alt='triangleBViolet'className='triangleViolet'></img><h1 className='reaTitle'>Nos réalisations</h1>
  </div>
@@ -25,7 +36,7 @@ export default function Realisations(){
       
       <div className="column">
  <a href='https://melaniemdm.github.io/testOn/' target="_blank" rel='noreferrer'>  <img src={img2} alt="Image1" width="100%" /></a>
- <a href={dsn} target="_blank" rel='noreferrer'> <img src={img6} alt="Image2" width="100%" /></a>
+ <SimpleModal text='' options={{bgColor:'white',width:'95%',height:'95%', imgUrl:pictureProjet, zIndex:'110'}}/> <img src={img6} alt="Image2" width="100%" onClick={()=>urlModal(dsn)}/>
     <a href='https://intelligence-canine.fr/' target="_blank" rel='noreferrer'>  <img src={img3} alt="Image3" width="100%" /></a> 
       
      
@@ -34,13 +45,13 @@ export default function Realisations(){
       <a href='https://melaniemdm.github.io/vendTout/' target="_blank" rel='noreferrer'> <img src={img11} alt="Image10" width="100%" /></a>
        <a href='http://www.grooving-family.com/photos.html' target="_blank" rel='noreferrer'>  <img src={img8} alt="Image4" width="100%" /></a>
        <a href=' https://melaniemdm.github.io/animals-love/' target="_blank" rel='noreferrer'>  <img src={img7} alt="Image5" width="100%" /></a> 
-      <a href={simpliste} target="_blank" rel='noreferrer'><img src={img10} alt="Image6" width="100%" /></a> 
-      <a href={acoss} target="_blank" rel='noreferrer'><img src={img4} alt="Image7" width="100%" /></a> 
+       <SimpleModal text='' options={{bgColor:'white',width:'95%',height:'95%', imgUrl:pictureProjet, zIndex:'110'}}/><img src={img10} alt="Image6" width="100%" onClick={()=>urlModal(simpliste)}/>
+       <SimpleModal text='' options={{bgColor:'white',width:'95%',height:'95%', imgUrl:pictureProjet, zIndex:'110'}}/><img src={img4} alt="Image7" width="100%" onClick={()=>urlModal(acoss)}/>
      
       </div>
       <div className="column">
       <a href='https://adaptativeresume.netlify.app' target="_blank" rel='noreferrer'> <img src={img9} alt="Image8" width="100%" /></a>
-      <a href={nature} target="_blank" rel='noreferrer'> <img src={img5} alt="Image9" width="100%" /></a>
+      <SimpleModal text='' options={{bgColor:'white',width:'95%',height:'95%', imgUrl:pictureProjet, zIndex:'110'}}/><img src={img5} alt="Image9" width="100%" onClick={()=>urlModal(nature)}/>
      <a href='https://melaniemdm.github.io/vendTout/' target="_blank" rel='noreferrer'> <img src={img1} alt="Image10" width="100%" /></a> 
      
       </div>     
