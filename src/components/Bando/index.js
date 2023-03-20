10ee8b581e6ea13cdeae45c5f161b7a5f9ea76c8
melaniemdm <melanieborja.pro@gmail.com>
2023-03-20 11:09:46 +0100

    fix(js & css) bando boucle & img add(font) courierPrime fix(css) font nav

--- src/components/Bando/index.js
+++ src/components/Bando/index.js
@@ -1,13 +1,17 @@
 import './style.scss';
-
+import bando from '../../assets/bando.png';
 export default function Bando(){
     return <div id='containerBando'>
-        
-        <div className="defileParent">
-<span className="defile" id="txtBando"
-data-text=" Digital o Création o Web o Graphisme o  &emsp; "> 
- &emsp; Digital o Création o Web o Graphisme o
-</span>
-</div>
+
+        {/* <div className="defileParent">
+
+<img className="defile" id="txtBando" src={bando} alt="bando"/>
+</div> */}
+
+<div class="marquee-rtl">
+<img className="defile" id="txtBando" src={bando} alt="bando"/>
+      </div>
+
 </div>
 }
+
